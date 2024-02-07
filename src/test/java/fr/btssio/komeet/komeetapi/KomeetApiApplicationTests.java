8e61cd02975fae5a52cacb1a4e125bac0a869490
Joris Thivard <joris.thivard@hotmail.com>
2024-02-07 20:38:21 +0100

    Initial commit

---
+++ src/test/java/fr/btssio/komeet/komeetapi/KomeetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.btssio.komeet.komeetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KomeetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
