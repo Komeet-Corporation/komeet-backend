b687e3f86f00b1c6b61bf7fb335b6c87057d959b
Joris Thivard <joris.thivard@hotmail.com>
2024-09-14 11:44:11 +0200

    [LOG] Startup logging info + actuator removed

--- src/main/java/fr/btssio/komeet/komeetapi/KomeetApiApplication.java
+++ src/main/java/fr/btssio/komeet/komeetapi/KomeetApiApplication.java
@@ -1,11 +1,15 @@
 package fr.btssio.komeet.komeetapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
+@Slf4j
 @SpringBootApplication
 public class KomeetApiApplication extends SpringBootServletInitializer {
 
@@ -15,7 +19,13 @@ public class KomeetApiApplication extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(KomeetApiApplication.class, args);
-    }
+        ConfigurableApplicationContext context = SpringApplication.run(KomeetApiApplication.class, args);
+        Environment env = context.getEnvironment();
 
+        log.info("==================================================");
+        log.info("{} started on version {}",
+                env.getProperty("application.name"),
+                env.getProperty("application.version"));
+        log.info("==================================================");
+    }
 }
