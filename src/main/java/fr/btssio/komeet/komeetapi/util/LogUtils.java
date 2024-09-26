5f0b6c3db9f5877a4c2258f4e0cd0e785ade29e9
Joris Thivard <joris.thivard@hotmail.com>
2024-09-26 17:27:11 +0200

    [LOG] refactor

--- src/main/java/fr/btssio/komeet/komeetapi/util/LogUtils.java
+++ src/main/java/fr/btssio/komeet/komeetapi/util/LogUtils.java
@@ -1,23 +1,25 @@
 package fr.btssio.komeet.komeetapi.util;
 
-import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Slf4j
 public class LogUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(LogUtils.class);
 
     private LogUtils() {
     }
 
     public static <T> void logInfo(@NotNull Class<T> tClass, String message) {
-        log.info("{} | {}", tClass.getName(), message);
+        log.info("{} | {}", tClass.getSimpleName(), message);
     }
 
     public static <T> void logWarn(@NotNull Class<T> tClass, String message) {
-        log.warn("{} | {}", tClass.getName(), message);
+        log.warn("{} | {}", tClass.getSimpleName(), message);
     }
 
     public static <T> void logError(@NotNull Class<T> tClass, String message, Throwable e) {
-        log.error("{} | {}", tClass.getName(), message, e);
+        log.error("{} | {}", tClass.getSimpleName(), message, e);
     }
 }
